3d77a66ceaed870d420faeb8bd8f2da655a527d7
Shashank Rai <saurabhrai.cec@gmail.com>
2022-01-10 04:12:29 -0800

    added header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import {GlobalStyle} from './GlobalStyle';
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
+      <Header />
       Start here.
+      <GlobalStyle />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// config
+
+// Components
+
+// Hooks
+
+// Fall Back Image
+import NoImage from '../images/no_image.js'
